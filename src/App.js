21593f7b05406954b62d3f3b6cacbfcf4b451c3a
Chava888 <salvador_tena@hotmail.com>
2022-02-02 09:45:24 -0800

    minor edits to css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CatEdit from './pages/CatEdit';
-import CatIndex from './pages/CatIndex';
-import CatNew from './pages/CatNew';
-import CatShow from './pages/CatShow';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import cats from './mockCat';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CatEdit from "./pages/CatEdit";
+import CatIndex from "./pages/CatIndex";
+import CatNew from "./pages/CatNew";
+import CatShow from "./pages/CatShow";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import cats from "./mockCat";
 
 export default class App extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      cats: cats
-    }
+      cats: cats,
+    };
   }
 
   render() {
-    console.log(cats)
+    console.log(cats);
     return (
-    <Router>
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/catindex" component={CatIndex} />
-        <Route path="/catshow" component={CatShow} />
-        <Route path="/catnew" component={CatNew} />
-        <Route path="/catedit" component={CatEdit} />
-        <Route component={NotFound}/>
-      </Switch>
-      <Footer/>
-  </Router>
-  )
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/catindex" component={CatIndex} />
+          <Route path="/catshow" component={CatShow} />
+          <Route path="/catnew" component={CatNew} />
+          <Route path="/catedit" component={CatEdit} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    );
   }
 }
-
