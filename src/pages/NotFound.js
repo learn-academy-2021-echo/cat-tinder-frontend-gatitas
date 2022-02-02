a6594b9a2f449ff8e7d685bd14a06bbe5984a13a
Chava888 <salvador_tena@hotmail.com>
2022-02-01 18:48:22 -0800

    added link component from react-dom

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './NotFound.css'
 
 export default class NotFound extends Component {
   render() {
-    return <div>
-         <h1>Not Found</h1>
+    return <div id="NotFound-container">
+         <h1>Not Found 404</h1>
     </div>;
   }
 }
