d7cdec0969effbc08220983aed5462b27720c1e4
AntonioAlgarin <antonio@cybermindedvets.com>
2022-02-01 18:05:25 -0800

    modified header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import {Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText} from 'reactstrap';
 
 export default class Header extends Component {
   render() {
-    return <div>
-        <h1>Header here</h1>
-    </div>;
+    return (
+    <h1>this is the header</h1>
+    // <div>
+    // <Navbar
+    //   color="primary"
+    //   dark
+    //   expand="md"
+    //   fixed="top"
+    //   light
+    // >
+    //   <NavbarBrand href="/">
+    //     Cat Tinder
+    //   </NavbarBrand>
+    //   <NavbarToggler onClick={function noRefCheck(){}} />
+    //   <Collapse navbar>
+    //     <Nav
+    //       className="me-auto"
+    //       navbar
+    //     >
+    //       <NavItem>
+    //         <NavLink href="/catshow/">
+    //           View Cats
+    //         </NavLink>
+    //       </NavItem>
+    //       <NavItem>
+    //         <NavLink href="https://github.com/reactstrap/reactstrap">
+    //           About Me
+    //         </NavLink>
+    //       </NavItem>
+    //       <UncontrolledDropdown
+    //         inNavbar
+    //         nav
+    //       >
+    //         <DropdownToggle
+    //           caret
+    //           nav
+    //         >
+    //           Matches
+    //         </DropdownToggle>
+    //         <DropdownMenu right>
+    //           <DropdownItem>
+    //             Most Recent
+    //           </DropdownItem>
+    //           <DropdownItem>
+    //             View Messages
+    //           </DropdownItem>
+    //           <DropdownItem divider />
+    //           <DropdownItem>
+    //             View Likes
+    //           </DropdownItem>
+    //         </DropdownMenu>
+    //       </UncontrolledDropdown>
+    //     </Nav>
+    //     <NavbarText>
+    //       Log Out
+    //     </NavbarText>
+    //   </Collapse>
+    // </Navbar>
+    // </div>
+  )
   }
 }
