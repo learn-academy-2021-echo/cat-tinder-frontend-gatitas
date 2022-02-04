98255cb61fc2fa72fc23029b2ef5381438987fc6
Chava888 <salvador_tena@hotmail.com>
2022-02-04 11:58:40 -0800

    changes to css files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Router, NavLink } from "react-router-dom";
+import "./Header.css";
 // import catLogo from "../assets/cat-logo.png";
 
 class Header extends Component {
